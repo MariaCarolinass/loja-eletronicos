7f5e98fdb081f7eb0b7ef8ebdbb803e9906f2e17
Carolina Soares <m.carolina.soares1@gmail.com>
2021-02-09 19:53:52 -0300

    Subindo código

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -14,7 +14,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/dbassistencia";
     private static final String USER = "root";
-    private static final String PASS = "carol94210134";
+    private static final String PASS = "senha";
     
     public static Connection getConnection() {
         try {
